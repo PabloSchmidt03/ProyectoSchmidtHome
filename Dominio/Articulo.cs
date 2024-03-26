6511ef4102a2b3e8be5658cb98a61450f2a6dbe7
Pablo <p.schmidt3141@gmail.com>
2024-03-25 21:36:23 -0300

    Dominio creado

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Articulo
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string UrlImagen { get; set; }
+        public int Precio { get; set; }
+
+        public Categoria Categoria  { get; set; }
+
+        //se trabaja con el atributo "color", pero en la base de datos se toma en cuenta como "marca", con el fin de no modificar la db
+        public Color color { get; set; }
+    }                                      
+}
