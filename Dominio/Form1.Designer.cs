602f7fb8ca679472c2dd17a48bc52dfd12bd00c9
Pablo <p.schmidt3141@gmail.com>
2024-03-25 21:46:19 -0300

    Primer formulario

--- Dominio/Form1.Designer.cs
+++ Dominio/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dominio
 {
-    partial class Form1
+    partial class frmInicio
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1025, 559);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1041, 598);
+            this.MinimumSize = new System.Drawing.Size(1041, 598);
+            this.Name = "frmInicio";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Schmidt Home";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
